f122c177506c3150a68cdf564f08771aced298c5
Nam Nguyen <namnm1.6@gmail.com>
2023-11-03 10:59:29 +0700

    add comment

--- src/lab14/PrimitiveCalculator.java
+++ src/lab14/PrimitiveCalculator.java
@@ -19,6 +19,9 @@ public class PrimitiveCalculator {
 		return sequence;
 	}
 
+
+// comment
+
 	public static void main(String[] args) {
 		try (Scanner scanner = new Scanner(System.in)) {
 			int n = scanner.nextInt();
